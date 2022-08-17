1dea813d599b33b4e1c4534ad84a22e4396ab5fe
Cole Gibbs <colegibbs0@gmail.com>
2022-08-17 11:55:57 -0700

    style import and state function

--- cut-cover-crush/src/App.js
+++ cut-cover-crush/src/App.js
@@ -1,8 +1,18 @@
 import Header from './components/Header'
+import './style/App.css'
+import {useState} from 'react'
 
 function App() {
+  const [choice, setChoice] = useState('')
+
+  function handleChoice(userChoice) {
+    setChoice(userChoice)
+  }
+
   return (
-    <Header/>
+    <main>
+      <Header/>
+    </main>
   );
 }
 
