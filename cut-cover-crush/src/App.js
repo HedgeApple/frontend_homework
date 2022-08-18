e0ac89b66858b5e14914f2db4216190abbf38420
Cole Gibbs <colegibbs0@gmail.com>
2022-08-17 23:26:30 -0700

    functionality complete

--- cut-cover-crush/src/App.js
+++ cut-cover-crush/src/App.js
@@ -1,45 +1,64 @@
-import Header from './components/Header'
-import WeaponForm from './components/WeaponForm'
-import Players from './components/Players'
-import './style/App.css'
-import {useState} from 'react'
+import Header from './components/Header';
+import WeaponForm from './components/WeaponForm';
+import Players from './components/Players';
+import './style/App.css';
+import {useState} from 'react';
 
 function App() {
   const [choice, setChoice] = useState('');
   const [wins, setWins] = useState(0);
   const [draws, setDraws] = useState(0);
   const [loses, setLoses] = useState(0);
+  const [compChoice, setCompChoice] = useState('');
+
 
   function handleChoice(userChoice) {
-    setChoice(userChoice)
+    setChoice(userChoice);
+    setCompChoice(randomChoice);
+    handleScore(choice, compChoice);
   }
 
-  function handleScore(score) {
-    if (score === "win"){
+  function handleScore(choice, compChoice) {
+    if (choice === '' || compChoice === '') {
+      return;
+    }
+
+    if (choice === "Rock" && compChoice === "Scissors"){
       setWins(wins + 1);
     }
-    else if (score === "draw") {
+    else if (choice === "Paper" && compChoice === "Rock") {
+      setWins(wins + 1);
+    }
+    else if(choice === "Scissors" && compChoice === "Paper") {
+      setWins(wins + 1);
+    }
+    else if (choice === compChoice) {
       setDraws(draws + 1);
     }
-    else if (score === "lose") {
+    else{
       setLoses(loses + 1);
     }
+  }
+
+  function randomChoice(){
+    let options = ["Rock", "Paper", "Scissors"];
+    return options[Math.floor(Math.random() * options.length )];
   }
 
   return (
     <main>
       <Header/>
-      <WeaponForm/>
+      <WeaponForm handleChoice={handleChoice}/>
 
-      <div class="flex-around">
+      <div className="flex-around">
         <h3>Wins: {wins}</h3>
         <h3>Draws: {draws}</h3>
         <h3>Loses: {loses}</h3>
       </div>
 
-      <div class="flex-around">
-        <Players player="You"/>
-        <Players player="The Enemy"/>
+      <div className="flex-around">
+        <Players player="You" choice={choice}/>
+        <Players player="The Enemy" choice={compChoice}/>
       </div>
     </main>
   );
