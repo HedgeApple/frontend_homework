3df1933a2d9d469bfc336a55d5a78cea7e779ea1
Cole Gibbs <colegibbs0@gmail.com>
2022-08-17 23:27:05 -0700

    minor changes

--- cut-cover-crush/src/components/Players.js
+++ cut-cover-crush/src/components/Players.js
@@ -3,7 +3,7 @@ import Card from 'react-bootstrap/Card';
 export default function Players(props) {
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img variant="top"/>
       <Card.Body>
         <Card.Title>{props.player}: {props.choice}</Card.Title>
       </Card.Body>
