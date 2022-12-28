accaddb7b090bd57f05dd1c7ccbf432e089a92e7
Nico Julian <NicoGiuliani@users.noreply.github.com>
2022-12-27 22:28:45 -0800

    Set up state for scores

--- rock-paper-scissors/src/components/Game.js
+++ rock-paper-scissors/src/components/Game.js
@@ -2,23 +2,46 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHandBackFist } from "@fortawesome/free-solid-svg-icons";
 import { faHandScissors } from "@fortawesome/free-solid-svg-icons";
 import { faHand } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Game = (props) => {
-  let playerSelection;
+  const [counter, setCounter] = useState(1);
+  const [playerWinCount, setPlayerWinCount] = useState(0);
+  const [opponentWinCount, setOpponentWinCount] = useState(0);
+  const [drawCount, setDrawCount] = useState(0);
 
   const buttonClicked = (e) => {
-    playerSelection = e.currentTarget.value;
+    const playerSelection = e.currentTarget.value;
     playRound(playerSelection);
   };
 
   const playRound = (playerSelection) => {
     const opponentSelection = opponentChoice();
     alert(
-      "Round #1: \nYou chose: " +
+      "Round #" +
+        counter +
+        ": \nYou chose: " +
         playerSelection +
         "\n Opponent chose: " +
         opponentSelection
     );
+
+    const matchOutcome = checkMatchOutcome(playerSelection, opponentSelection);
+    switch (matchOutcome) {
+      case "PLAYER":
+        setPlayerWinCount(playerWinCount + 1);
+        break;
+      case "OPPONENT":
+        setOpponentWinCount(opponentWinCount + 1);
+        break;
+      case "DRAW":
+        setDrawCount(drawCount + 1);
+        break;
+      default:
+        break;
+    }
+
+    setCounter(counter + 1);
   };
 
   const opponentChoice = () => {
@@ -27,8 +50,33 @@ const Game = (props) => {
     return choices[random_index];
   };
 
+  const checkMatchOutcome = (playerChoice, opponentChoice) => {
+    const winningCombinations = {
+      ROCK: "SCISSORS",
+      PAPER: "ROCK",
+      SCISSORS: "PAPER",
+    };
+
+    if (
+      opponentChoice.toUpperCase() ===
+      winningCombinations[playerChoice.toUpperCase()]
+    ) {
+      return "PLAYER";
+    } else if (playerChoice === opponentChoice) {
+      return "DRAW";
+    } else {
+      return "OPPONENT";
+    }
+  };
+
   return (
     <div className="col-12 text-center">
+      <div className="mb-5" id="scoreboard">
+        <h1 className="mb-3">Scoreboard</h1>
+        <h2>Player: {playerWinCount}</h2>
+        <h2>Opponent: {opponentWinCount}</h2>
+        <h2>Draw: {drawCount}</h2>
+      </div>
       <div className="card border-success mb-3">
         <div className="card-header">Rock Paper Scissors</div>
         <div className="card-body">
